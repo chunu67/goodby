1ca03634d25cac771359f551e155aba5adfd5e92
chunu67 <chunu@gitlab.com>
2020-11-27 18:39:21 +0530

    add all images with /img directory

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const images = document.querySelectorAll("span");
 
+flowers=['https://www.pexels.com/search/flowers/','https://unsplash.com/images/nature/flower','https://burst.shopify.com/flowers']
+
+
 images.forEach((image, index) => {
-  image.addEventListener("click", e => {
+  image.addEventListener("click",e => {
+	//window.location.href = e.path[0].childNodes[0].currentSrc ;
+	//images[0].childNodes[0].currentSrc;
+   	console.log(e.path[0].childNodes[0].currentSrc);
+	//console.log(e);
+
+  });
+  image.addEventListener('mouseover', e => {
+	setTimeout(function(){
     anime({
       targets: images,
       autoplay: true,
@@ -22,11 +33,16 @@ images.forEach((image, index) => {
       { value: 1, easing: "easeInOutQuad", duration: 1100 }],
 
       delay: anime.stagger(200, { grid: [5, 5], from: index }) });
-
+	},2000);
   });
 });
 
+
 //window.onload = () => document.querySelector('h1').remove();
+
+
+
+
 
 //window.onload = () => document.getElementById('remove').remove();
 document.addEventListener("DOMContentLoaded", () => {

